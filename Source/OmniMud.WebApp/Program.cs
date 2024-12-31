57d9758a79d37b97fcc97e44de0fe0bba53200da
Michael Bledsoe <michael.bledsoe@gmail.com>
2024-12-31 10:03:54 -0500

    Initial web app commit

---
+++ Source/OmniMud.WebApp/Program.cs
@@ -0,0 +1,20 @@
+namespace OmniMud.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddMvc();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
